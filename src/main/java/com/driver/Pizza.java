cd3fdbbe609d17618941a460be8e087dc4ef24ea
Aditya Zore <adityazore00@gmail.com>
2026-06-27 21:28:13 +0530

    First commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,33 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
+    private boolean isVeg;
     private String bill;
+    private int total;
+
+    private boolean extraCheeseAdded=false;
+    private boolean extraToppingAdded=false;
+
+
+    private int extraCheesePrice=80;
+    private int extraTopping;
+    private boolean extraTake=false;
+    private int paperBag=20;
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price=300;
+            extraTopping=70;
+        }
+        else{
+            this.price=400;
+            extraTopping=120;
+        }
+        total=price;
     }
 
     public int getPrice(){
@@ -17,18 +38,49 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!extraCheeseAdded){
+            total=total+extraCheesePrice;
+            extraCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!extraToppingAdded){
+            total=total+extraTopping;
+            extraToppingAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!extraTake){
+            total=total+paperBag;
+            extraTake=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        String bill = "";
+
+        bill += "Base Price Of The Pizza: " + price + "\n";
+
+        if (extraCheeseAdded) {
+            bill += "Extra Cheese Added: " + extraCheesePrice + "\n";
+        }
+
+        if (extraToppingAdded) {
+            bill += "Extra Toppings Added: " + extraToppingAdded + "\n";
+        }
+
+        if (extraTake) {
+            bill += "Paperbag Added: " + paperBag + "\n";
+        }
+
+        bill += "Total Price: " + total;
+
+
+        return bill;
     }
 }
